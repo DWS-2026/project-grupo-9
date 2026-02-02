428d3798bb63c61ab40a531e306479968f538028
Ainoa-AS <a.acosta.2024@alumnos.urjc.es>
2026-02-02 20:49:01 +0100

    Primera version Barra de Navegacion y Footer
    Creación de footer.js, que se encarga de crear el footer de la web y de navBar.js que se encarga de crear la barra de navegación. El objetivo de este commit es unificar código y que no se repita.

---
+++ footer.js
@@ -0,0 +1,6 @@
+let foot = document.getElementById('footer');
+foot.innerHTML = `<footer class="py-5 bg-dark">
+                    <div class="container">
+                        <p class="m-0 text-center text-white">Copyright &copy; Grupo 9</p>
+                    </div>
+                </footer>`
---
+++ navBar.js
@@ -0,0 +1,31 @@
+let bar = document.getElementById('navigationBar');
+
+bar.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #dfc797;">
+                    <div class="container px-4 px-lg-5">
+                        <a class="navbar-brand fw-bold" href="#!">
+                            BOMBOMB
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation"><span
+                            class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                                <li class="nav-item fw-bold"><a class="nav-link" aria-current="page" href="#!">HOME</a>
+                                </li>
+                                <li class="nav-item fw-bold"><a class="nav-link" href="#!">ABOUT</a></li>
+                                <li class="nav-item fw-bold"><a class="nav-link" href="#!">PRODUCTS</a></li>
+                                <li class="nav-item fw-bold"><a class="nav-link" href="#!">STORE</a></li>
+                            </ul>
+                            <a class="btn btn-outline-dark me-1" href="./cart.html">
+                                <i class="bi-cart-fill me-1"></i>
+                                Carrito
+                                <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                            </a>
+                            <a class="btn btn-outline-dark me-1" href="./profilePage.html">
+                                <i class="bi-person-fill me-1"></i>
+                                Perfil
+                            </a>
+                        </div>
+                    </div>
+                </nav>`;

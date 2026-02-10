03d2722e6127fe24e739cb195b3371061c4f6985
angelaurjc <a.briceno.2024@alumnos.urjc.es>
2026-02-10 10:11:18 +0100

    Controlador productos
    He creado el controlador de productos con el mapping de productsPage, createProduct, editProductPage y productDetailsPage.

---
+++ Bombomb/src/main/java/es/codeurjc/web/ProductController.java
@@ -0,0 +1,29 @@
+package main.java.es.codeurjc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+    @GetMapping("/products")
+	public String products(Model model) {
+		return "productsPage_template";
+	}
+
+    @GetMapping("/createproduct")
+	public String createProduct(Model model) {
+		return "createProduct_template";
+	}
+
+    @GetMapping("/editproduct")
+	public String editProduct(Model model) {
+		return "editProductPage_template";
+	}
+
+    @GetMapping("/productdetails")
+	public String productDetails(Model model) {
+		return "productDeatilsPage_template";
+	}
+}
+

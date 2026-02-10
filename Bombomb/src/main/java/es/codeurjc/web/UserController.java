e0c805aa9a11e8d06dd87f1e4c9e480ae3989013
Ainoa-AS <a.acosta.2024@alumnos.urjc.es>
2026-02-10 10:03:47 +0100

    Creación UserController
    He creado user controller y he creado el mapping de profilePage, signIn, editProfile, logIn.

---
+++ Bombomb/src/main/java/es/codeurjc/web/UserController.java
@@ -0,0 +1,30 @@
+package main.java.es.codeurjc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/profile")
+	public String profile(Model model) {
+		return "profilePage_template";
+	}
+
+    @GetMapping("/login")
+	public String profile(Model model) {
+		return "logInPage_template";
+	}
+
+    @GetMapping("/signin")
+	public String profile(Model model) {
+		return "signInPage_template";
+	}
+
+    @GetMapping("/editprofile")
+	public String profile(Model model) {
+		return "editProfile_template";
+	}
+}
+
+

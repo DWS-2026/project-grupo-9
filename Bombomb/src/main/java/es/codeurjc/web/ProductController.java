2e4925b7cdc14e4e5077a5eb3691282b28cab70b
angelaurjc <a.briceno.2024@alumnos.urjc.es>
2026-02-15 18:22:02 +0100

    Ajuste en el controlador de productos y páginas de producto y detalles.
    Ahora los ejemplos con el bombón "Violeta" funcionan con los atributos que gestiona el controlador de productos.

--- Bombomb/src/main/java/es/codeurjc/web/ProductController.java
+++ Bombomb/src/main/java/es/codeurjc/web/ProductController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProductController {
     @GetMapping("/products")
 	public String products(Model model) {
-		model.addAttribute("name", "World");
+		model.addAttribute("name", "Violeta");
+		model.addAttribute("price", "0.60€");
+		model.addAttribute("image", "images/chocolate_flower.jpeg");
 		return "productsPage";
 	}
 
@@ -25,6 +27,11 @@ public class ProductController {
 
     @GetMapping("/productdetails")
 	public String productDetails(Model model) {
+		model.addAttribute("name", "Violeta");
+		model.addAttribute("price", "0.60€");
+		model.addAttribute("description", "Bombón en forma de flor relleno de moras y brillo metalizado");
+		model.addAttribute("stock", "12");
+		model.addAttribute("image", "images/chocolate_flower.jpeg");
 		return "productDetailsPage";
 	}
 

84c90e7da69455257d1e00acda60cd8f10800345
Ainoa-AS <a.acosta.2024@alumnos.urjc.es>
2026-02-16 19:49:31 +0100

    Cambio UserController

--- Bombomb/src/main/java/es/codeurjc/web/UserController.java
+++ Bombomb/src/main/java/es/codeurjc/web/UserController.java
@@ -1,7 +1,5 @@
 package es.codeurjc.web;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;

c5aeba84e20e35cfc105ece05df7aba53d155b65
Ainoa-AS <a.acosta.2024@alumnos.urjc.es>
2026-02-17 10:10:59 +0100

    Modificación Chocolate

--- Bombomb/src/main/java/es/codeurjc/web/Chocolate.java
+++ Bombomb/src/main/java/es/codeurjc/web/Chocolate.java
@@ -3,16 +3,9 @@ package es.codeurjc.web;
 import org.springframework.stereotype.Indexed;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
 public class Chocolate extends Product{
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
 
     protected Chocolate(){}
 

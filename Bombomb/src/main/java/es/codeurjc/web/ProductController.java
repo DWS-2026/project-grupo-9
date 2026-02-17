933931e1a3d7c9541a5ba32512779f43be6fb1d6
Ainoa-AS <a.acosta.2024@alumnos.urjc.es>
2026-02-17 20:37:53 +0100

    Inclusión de la base de datos en el controlador de productos
    He conectado el controlador de productos con la base de datos y he metido algunos productos de prueba para probar si funciona.

--- Bombomb/src/main/java/es/codeurjc/web/ProductController.java
+++ Bombomb/src/main/java/es/codeurjc/web/ProductController.java
@@ -1,11 +1,27 @@
 package es.codeurjc.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import jakarta.annotation.PostConstruct;
 
 @Controller
 public class ProductController {
+
+	@Autowired
+	ProductRepository products;
+
+	@PostConstruct
+	public void init(){
+		products.save(new Product("Violeta", "0.60€",
+		 "Bombón en forma de flor relleno de moras y brillo metalizado",
+		  "12", "/images/chocolate_flower.jpeg", "Bombón"));
+		products.save(new Product("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaa"));
+	}
+
     @GetMapping("/products")
 	public String products(Model model) {
 		model.addAttribute("name", "Violeta");
@@ -30,13 +46,10 @@ public class ProductController {
 		return "editProductPage";
 	}
 
-    @GetMapping("/productdetails")
-	public String productDetails(Model model) {
-		model.addAttribute("name", "Violeta");
-		model.addAttribute("price", "0.60€");
-		model.addAttribute("description", "Bombón en forma de flor relleno de moras y brillo metalizado");
-		model.addAttribute("stock", "12");
-		model.addAttribute("image", "images/chocolate_flower.jpeg");
+    @GetMapping("/product/{id}/details")
+	public String productDetails(Model model, @PathVariable long id) {
+		Product product = products.getById(id);
+		model.addAttribute("product", product);
 		return "productDetailsPage";
 	}
 

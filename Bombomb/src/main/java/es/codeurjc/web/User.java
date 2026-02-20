3767abd7bba4212a702acf609b525df57c75993a
iness1899 <i.ucles.2024@alumnos.urjc.es>
2026-02-20 22:50:30 +0100

    Clase User y repositorio empezada

---
+++ Bombomb/src/main/java/es/codeurjc/web/User.java
@@ -0,0 +1,90 @@
+package es.codeurjc.web;
+
+import org.springframework.stereotype.Indexed;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String surname;
+    private String telephone;
+    private String email;
+    private String image;
+    private String password;
+
+    protected User(){}
+
+    public User(String name, String surname, String telephone, String email, String image, String password){
+        
+        this.name=name;
+        this.surname=surname;
+        this.telephone=telephone;
+        this.email=email;
+        this.image=image;
+        this.password=password;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "User[id=%d, name='%s', surname='%s', telephone='%s', email='%s', image='%s', password='%s']",
+                id, name, surname, telephone, email, image, password);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+     public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+     public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+     public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+     public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPassword() {/*no se hasta que punto esta funcion es buena idea */
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+   
+}

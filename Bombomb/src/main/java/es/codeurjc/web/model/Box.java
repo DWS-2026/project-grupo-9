9f44891653b22b44b28285e05444858f27172a92
sagarUr <s.garciaro.2024@alumnos.urjc.es>
2026-03-03 10:46:59 +0100

    clase Box con getters y setters

--- Bombomb/src/main/java/es/codeurjc/web/model/Box.java
+++ Bombomb/src/main/java/es/codeurjc/web/model/Box.java
@@ -2,15 +2,51 @@ package es.codeurjc.web.model;
 
 import java.sql.Blob;
 
+import java.util.List;
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Id;
 
 @Entity
 public class Box extends Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     private int size=9;
+    String name;
+
+    @Lob
+    Blob image;
+    
+    @ManyToMany
+    private List<Chocolate> chocolates;
 
     public Box(String name, String price, String description, Blob image, String type, Boolean madeByAdmin){
         super(name, price, description, image, "Box", madeByAdmin);
     }
 
+    
+    
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public Blob getImage() { 
+        return image;
+    }
+    public void setImage(Blob image) {
+        this.image = image;
+    }
+    public void setChocolateList(List<Chocolate> chocolates){
+        this.chocolates = chocolates;
+    }
+    public List<Chocolate> getChocolateList(){
+        return this.chocolates;
+    }
+    
 }

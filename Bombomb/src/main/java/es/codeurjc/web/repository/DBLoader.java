93ec36f704b88b2a0e1e5e484797f127ce347ac4
Ainoa-AS <a.acosta.2024@alumnos.urjc.es>
2026-03-06 12:04:02 +0100

    Cambio nombres de usuario
    He cambiado los nombres de usuario para que sean más sencillos

--- Bombomb/src/main/java/es/codeurjc/web/repository/DBLoader.java
+++ Bombomb/src/main/java/es/codeurjc/web/repository/DBLoader.java
@@ -23,77 +23,84 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class DBLoader {
 
-    @Autowired
-    private ChocolateRepository chocolateRepository;
+	@Autowired
+	private ChocolateRepository chocolateRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-    
-    @Autowired
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-    @PostConstruct
+	@PostConstruct
 	private void initDatabase() throws IOException, SerialException, SQLException {
 		ClassPathResource resource = new ClassPathResource("static/images/chocolate_flower.jpeg");
 		byte[] bytes = resource.getInputStream().readAllBytes();
 		Blob blob = new SerialBlob(bytes);
 		userRepository.save(new User("María de la O", "Sánchez Sánchez",
-				"600808080", "mariasanchezsanchez@hotmail.com", blob, 
+				"600808080", "mariasanchezsanchez@hotmail.com", blob,
 				passwordEncoder.encode("pass"), "USER"));
 
 		resource = new ClassPathResource("static/images/chocolate_pink.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		userRepository.save(new User("Administrador", "Adminis Trado",
-				"666 666 666", "adminstrador@gmail.com", blob, 
-				passwordEncoder.encode("adminpass"), "USER", "ADMIN"));
+				"666 666 666", "admin@gmail.com", blob,
+				passwordEncoder.encode("admin"), "USER", "ADMIN"));
 
-        resource = new ClassPathResource("static/images/chocolate_flower.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_pink.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
-		chocolateRepository.save(new Chocolate("Violeta",blob));
+		userRepository.save(new User("Usu", "Ario",
+				"000 000 000", "user@gmail.com", blob,
+				passwordEncoder.encode("user"), "USER"));
 
-        resource = new ClassPathResource("static/images/chocolate_black.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_flower.jpeg");
+		bytes = resource.getInputStream().readAllBytes();
+		blob = new SerialBlob(bytes);
+		chocolateRepository.save(new Chocolate("Violeta", blob));
+
+		resource = new ClassPathResource("static/images/chocolate_black.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Espiral de Medianoche", blob));
 
-        resource = new ClassPathResource("static/images/chocolate_boat.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_boat.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Barca de Avellana", blob));
 
-        resource = new ClassPathResource("static/images/chocolate_cake.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_cake.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Triángulo de las delicias", blob));
 
-        resource = new ClassPathResource("static/images/chocolate_cube.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_cube.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Caramelo Imperial", blob));
 
-        resource = new ClassPathResource("static/images/chocolate_green.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_green.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Bosque de cacao", blob));
 
-        resource = new ClassPathResource("static/images/chocolate_lemon.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_lemon.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Delicia de Cítrico", blob));
 
-        resource = new ClassPathResource("static/images/chocolate_orange.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_orange.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Puesta de Sol", blob));
 
-        resource = new ClassPathResource("static/images/chocolate_pink_heart.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_pink_heart.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Corazón Silvestre", blob));
 
-        resource = new ClassPathResource("static/images/chocolate_pink.jpeg");
+		resource = new ClassPathResource("static/images/chocolate_pink.jpeg");
 		bytes = resource.getInputStream().readAllBytes();
 		blob = new SerialBlob(bytes);
 		chocolateRepository.save(new Chocolate("Mármol de frambuesa", blob));

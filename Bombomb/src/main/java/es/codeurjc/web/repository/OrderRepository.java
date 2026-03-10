2a40aa00cddf1d2b91d846433b332df3d1547f95
angelaurjc <a.briceno.2024@alumnos.urjc.es>
2026-03-10 09:34:33 +0100

    Ajustes para consultas OrderReposititory.

--- Bombomb/src/main/java/es/codeurjc/web/repository/OrderRepository.java
+++ Bombomb/src/main/java/es/codeurjc/web/repository/OrderRepository.java
@@ -3,7 +3,15 @@ package es.codeurjc.web.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.codeurjc.web.model.Order;
+import es.codeurjc.web.model.Box;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
     
+
+    List<Order> findByUserEmailAndIsOpen(String Email, Boolean isOpen);
+    List<Box> findByUserEmailAndIsOpenAndBoxIsOpenBox(String Email, Boolean isOpen, Boolean isOpenBox);
+
+
 }

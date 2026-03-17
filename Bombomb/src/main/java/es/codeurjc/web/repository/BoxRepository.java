9ae03418591ca9c379b6305a6d440b1f4985af9d
sagarUr <s.garciaro.2024@alumnos.urjc.es>
2026-03-17 16:16:56 +0100

    box repository

--- Bombomb/src/main/java/es/codeurjc/web/repository/BoxRepository.java
+++ Bombomb/src/main/java/es/codeurjc/web/repository/BoxRepository.java
@@ -3,10 +3,26 @@ package es.codeurjc.web.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import es.codeurjc.web.model.Box;
 
 public interface BoxRepository extends JpaRepository<Box, Long>{
 
-    Optional<Box> findByIsOpenBoxAndOrdersIsOpenAndOrdersUserEmail(Boolean isOpenBox, Boolean isOpen,  String email);
+    //Optional<Box> findByIsOpenBoxAndOrdersIsOpenAndOrdersUserEmail(Boolean isOpenBox, Boolean isOpen, String email);
+    @Query("""
+    SELECT b
+    FROM Box b
+    JOIN b.orders o
+    JOIN o.user u
+    WHERE b.isOpenBox = :isOpenBox
+      AND o.isOpen = :isOpen
+      AND u.email = :email
+""")
+Optional<Box> findBoxByStatusAndUserEmail(
+        @Param("isOpenBox") Boolean isOpenBox,
+        @Param("isOpen") Boolean isOpen,
+        @Param("email") String email
+);
 }

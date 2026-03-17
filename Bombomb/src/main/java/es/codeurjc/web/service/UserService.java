1dd13828f6cfa9679591ec01152f97de259c3579
iness1899 <i.ucles.2024@alumnos.urjc.es>
2026-03-17 11:09:58 +0100

    Validación contraseña (8 caracteres)

--- Bombomb/src/main/java/es/codeurjc/web/service/UserService.java
+++ Bombomb/src/main/java/es/codeurjc/web/service/UserService.java
@@ -1,13 +1,16 @@
 package es.codeurjc.web.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.sql.rowset.serial.SerialBlob;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import es.codeurjc.web.model.User;
@@ -16,10 +19,15 @@ import es.codeurjc.web.repository.UserRepository;
 @Service
 public class UserService {
     
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    private UserRepository userRepository;   
+    private UserRepository userRepository;
 
-     public void save(User user, MultipartFile image) throws IOException{
+    UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }   
+
+     public void save(User user, MultipartFile image,@RequestParam(required = true) String passwordString) throws IOException{
         if (!image.isEmpty()) {
 			try {
 				user.setImage(new SerialBlob(image.getBytes()));
@@ -27,8 +35,28 @@ public class UserService {
 				throw new IOException("Failed to create image blob", e);
 			}
 		}
-        userRepository.save(user);
+        if(minPasswordLength(passwordString)==false) {
+            throw new IOException("Password must be at least 8 characters long");
+        }
+     userRepository.save(user);
+
+    
     }
+    //////////////////////////////////////////////
+   
+   /*  if(user.getRoles().isEmpty()) {
+        List<String> cleanRoles = new ArrayList<>();
+        for (String role : user.getRoles()) {
+            if(role.startsWith("ROLE_")) {
+                cleanRoles.add(role.substring(5));
+            } else {
+                cleanRoles.add(role);
+            }
+            user.setRoles(cleanRoles);
+        }
+    }*/
+
+    /// //////////////////////////////////////////////
 
      public List<User> findAll(){
         return userRepository.findAll();
@@ -45,4 +73,7 @@ public class UserService {
         return userRepository.findByEmail(email);
     }
 
+    public boolean minPasswordLength(String password) {
+        return password.length() >= 8;
+    }
 }

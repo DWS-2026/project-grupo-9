3bbf8b4ef234a2fe38836d139ab9ce0d52e5735f
angelaurjc <a.briceno.2024@alumnos.urjc.es>
2026-04-14 09:41:50 +0200

    Configuración seguridad api rest.
    La configuración que hemos añadido y comentado con el profesor en clase.

--- Bombomb/src/main/java/es/codeurjc/web/SecurityConfiguration.java
+++ Bombomb/src/main/java/es/codeurjc/web/SecurityConfiguration.java
@@ -3,12 +3,16 @@ package es.codeurjc.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -32,7 +36,54 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	@Order(1)
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+
+		http.authenticationProvider(authenticationProvider());
+
+		http
+				.securityMatcher("/api/**");
+		//		.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+
+		http
+				.authorizeHttpRequests(authorize -> authorize
+						// PRIVATE ENDPOINTS
+						// Images
+						//.requestMatchers(HttpMethod.PUT, "/api/images/*/media").hasRole("USER")
+						//.requestMatchers(HttpMethod.DELETE, "/api/books/*/images/*").hasRole("USER")
+						// Books
+						//.requestMatchers(HttpMethod.POST, "/api/books/**").hasRole("USER")
+						//.requestMatchers(HttpMethod.PUT, "/api/books/**").hasRole("USER")
+						//.requestMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN")
+						// Shops
+						//.requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
+						//.requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
+						//.requestMatchers(HttpMethod.DELETE, "/api/shops/**").hasRole("ADMIN")
+						// PUBLIC ENDPOINTS
+						.anyRequest().permitAll());
+
+		// Disable Form login Authentication
+		http.formLogin(formLogin -> formLogin.disable());
+
+		// Disable CSRF protection (it is difficult to implement in REST APIs)
+		http.csrf(csrf -> csrf.disable());
+
+		// Disable Basic Authentication
+		http.httpBasic(httpBasic -> httpBasic.disable());
+
+		// Stateless session
+		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// Add JWT Token filter
+		//http.addFilterBefore(new JwtRequestFilter(userDetailService, jwtTokenProvider),
+		//		UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
         http.authenticationProvider(authenticationProvider());
         http
 			.authorizeHttpRequests(authorize -> authorize

37f0acec722fa65f8bccdca31204145961684819
Ainoa-AS <a.acosta.2024@alumnos.urjc.es>
2026-04-22 16:19:39 +0200

    Seguridad en API REST
    He añadido todo lo necesario para empezar con la seguridad en la API REST, además ya se puede empezar a usar las sesiones. El inicio de sesion está en api/v1/auth/login

--- Bombomb/src/main/java/es/codeurjc/web/security/SecurityConfiguration.java
+++ Bombomb/src/main/java/es/codeurjc/web/security/SecurityConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,6 +15,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import es.codeurjc.web.security.jwt.JwtTokenProvider;
+import es.codeurjc.web.security.jwt.JwtRequestFilter;
+import es.codeurjc.web.security.jwt.UnauthorizedHandlerJwt;
 
 
 @Configuration
@@ -23,10 +29,21 @@ public class SecurityConfiguration {
     @Autowired
     public RepositoryUserDetailsService userDetailsService;
 
+    @Autowired
+	private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+  	private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
@@ -42,8 +59,8 @@ public class SecurityConfiguration {
 		http.authenticationProvider(authenticationProvider());
 
 		http
-				.securityMatcher("/api/**");
-		//		.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+				.securityMatcher("/api/**")
+				.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
 
 		http
 				.authorizeHttpRequests(authorize -> authorize
@@ -75,8 +92,8 @@ public class SecurityConfiguration {
 		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 		// Add JWT Token filter
-		//http.addFilterBefore(new JwtRequestFilter(userDetailService, jwtTokenProvider),
-		//		UsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(new JwtRequestFilter(userDetailsService, jwtTokenProvider),
+				UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}

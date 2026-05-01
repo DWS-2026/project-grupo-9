13ab86843ec48cde88d2b66798ac1bf92e631479
Ainoa-AS <a.acosta.2024@alumnos.urjc.es>
2026-05-01 18:35:57 +0200

    Arreglo error retorno html
    He modificado estos dos archivos ya que no estan bien y por eso devolvía html la api rest en los errores

--- Bombomb/src/main/java/es/codeurjc/web/security/jwt/JwtRequestFilter.java
+++ Bombomb/src/main/java/es/codeurjc/web/security/jwt/JwtRequestFilter.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -15,6 +16,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 	
 	private static final Logger log = LoggerFactory.getLogger(JwtRequestFilter.class);
@@ -42,7 +44,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 			authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 		} catch (Exception ex) {
-			log.info(ex.getMessage());
+			//Avoid logging when no token is found
+			if(!ex.getMessage().equals("No access token cookie found in request")) {
+				log.error("Exception processing JWT Token: ", ex);
+			}			
 		}
 
 		filterChain.doFilter(request, response);
